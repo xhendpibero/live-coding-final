62fa0cc6865709462d05754d0742ef5def77fb5e
Dendy SA <dendysaptoadi160@gmail.com>
2025-10-28 23:46:50 +0700

    update

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -8,8 +8,8 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 1000 * 60 * 5, // 5 minutes
-        cacheTime: 1000 * 60 * 60, // 1 hour
+        staleTime: 1000 * 60 * 5,
+        gcTime: 1000 * 60 * 60,
       },
     },
   }))
